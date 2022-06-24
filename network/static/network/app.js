4a5f1ea6318217ddfc76f15d35485c9839f5d654
unknown <ortiztato@gmail.com>
2022-06-24 13:10:23 -0600

    modificado el js para que cierre el menu con el nametitle

--- network/static/network/app.js
+++ network/static/network/app.js
@@ -2,6 +2,7 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const followingBtn = document.querySelector("#following");
+const userBtn = document.querySelector("#nametitle");
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
@@ -10,3 +11,4 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 followingBtn.addEventListener("click", toggleMenu);
+userBtn.addEventListener("click", toggleMenu);
